4480496dc520f96edc3b53a90be4750e4068d17b
github username <github email address>
2021-04-13 11:39:03 -0400

    added sports selector testing fetch and adding event listener

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-// sports array
-const sports = []
+// sports objects
+const sports = document.getElementById('item')
 // odds array 
 const odds = [] 
 //container
-const n = document.getElementById('new')
-//test fetch 
-fetch('http://localhost:3000/conferences').then(r => r.json()).then(appendItems)
+const n = document.getElementById('item')
+//test fetch for initial load
+fetch('http://localhost:3000/sports').then(r => r.json()).then(appendItems)
 
 function appendItems(x) {
-    const conferences = x['data'];
-    const h1Elements = conferences.map(function(x){
-        const h1 = document.createElement('h1')
-        h1.innerText = `${x.attributes.league}`
-        return h1
-    })
-    const liElements = conferences.map(function(x){
+    const sports = x['data'];
+    const liElements = sports.map(function(x){
         const li = document.createElement('li')
-        li.innerText = `${x.attributes.conference} ${x.attributes.division}`
+        li.id = `${x.attributes.key}`
+        li.innerText = `${x.attributes.title} ${x.attributes.group}`
         li.className = 'wrapper'
         return li
     })
-    h1Elements.forEach(e => {
-        n.appendChild(e)
-    });
     liElements.forEach(e => {
         n.appendChild(e)
     });
 }
+
+
+sports.addEventListener('click', (event) => {
+    event.preventDefault()
+    console.log(event.target.id)
+    // fetch(`http://localhost:3000/sports/${event.target.id}`)
+})
+
+
 
 
 
