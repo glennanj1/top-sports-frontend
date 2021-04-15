36889c1cab9bdc5a65ab46bdb0295787819a12d9
glennanj1 <glennan20@gmail.com>
2021-04-14 22:53:53 -0400

    fixed containers, variable dublication and added  odds fetch

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-// sports objects
-const sports = document.getElementById('item')
-//container
-const n = document.getElementById('item')
+// sports container
+const sports = document.getElementById('container')
 // home button
 const home = welcome
-//test fetch for initial load
+//odds objects
+const odds = []
+//bets 
+const sites = []
+//sports fetch on initial load
 fetch('http://localhost:3000/sports').then(r => r.json()).then(appendItems)
 
 function appendItems(x) {
-    const sports = x['data'];
-    const liElements = sports.map(function(x){
-        const li = document.createElement('li')
-        li.id = `${x.id}`
-        li.innerText = `${x.attributes.title} ${x.attributes.group}`
-        li.className = 'wrapper'
-        return li
+    const s = x['data'];
+    
+    const divElements = s.map(function(x){
+        const div = document.createElement('div')
+        div.id = `${x.id}`
+        div.innerText = `${x.attributes.title}`
+        return div
     })
-    liElements.forEach(e => {
-        n.appendChild(e)
+    divElements.forEach(e => {
+        sports.appendChild(e)
     });
 }
 
-//sports fetch
+//games fetch
 sports.addEventListener('click', (event) => {
     event.preventDefault()
     // console.log(event.target.id)
@@ -29,12 +31,13 @@ sports.addEventListener('click', (event) => {
     return fetch(`http://localhost:3000/sports/${event.target.id}/games`).then(r => r.json()).then(appendSports)
 
     function appendSports(x) {
-    n.style.display = 'none'
+    sports.style.display = 'none'
     const newItem = document.createElement('div')
     newItem.id = 'game'
-    const container = document.querySelector("#container")
+    const container = document.querySelector("#gameContainer")
     container.append(newItem)
     const games = x['data']
+    debugger;
     const liElements = games.map(function(x){
         const li = document.createElement('li')
         li.id = `${x.id}`
@@ -45,31 +48,48 @@ sports.addEventListener('click', (event) => {
     liElements.forEach(e => {
         newItem.appendChild(e)
     });
+
     
 
     }
 })
+
+//odd fetch
+gameContainer.addEventListener('click', (event) => {
+    console.log(event.target.id)
+
+    return fetch(`http://localhost:3000/sports/${event.target.id}/games`).then(r => r.json()).then(appendSports)
+
+    function appendSports(x) {
+    game.remove()
+    const newItem = document.createElement('div')
+    newItem.id = 'odds'
+    const container = document.querySelector("#oddsContainer")
+    container.append(newItem)
+    const games = x['data']
+    debugger;
+    const liElements = games.map(function(x){
+        const li = document.createElement('li')
+        li.id = `${x.id}`
+        li.innerText = `${x.attributes.teams} ${x.attributes['sport_nice']}`
+        li.className = 'wrapper'
+        return li
+    });
+    liElements.forEach(e => {
+        newItem.appendChild(e)
+    });
+    }
+})
+
+
 //home button
 home.addEventListener('click', (e) => {
     console.log(e)
     e.preventDefault()
     game.remove()
-    n.style.display = 'block'
+    sports.style.display = 'block'
     
 })
 
 //sidebar
 const form = document.querySelector("body > div.sidebar > form")
-
-
-
-
-
-
-
-
-
-
-
-
-
