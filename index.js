f0b611c41531840c1dc5308cb820d6cf45da267e
glennanj1 <glennan20@gmail.com>
2021-04-15 14:34:52 -0400

    made loadsports function and hoisted function call also added odds logic and fixed home button to correctly render all sports without duplication

--- index.js
+++ index.js
@@ -7,37 +7,40 @@ const odds = []
 //bets 
 const sites = []
 //sports fetch on initial load
-fetch('http://localhost:3000/sports').then(r => r.json()).then(appendItems)
+loadSports()
 
-function appendItems(x) {
-    const s = x['data'];
-    
-    const divElements = s.map(function(x){
-        const div = document.createElement('div')
-        div.id = `${x.id}`
-        div.innerText = `${x.attributes.title}`
-        return div
-    })
-    divElements.forEach(e => {
-        sports.appendChild(e)
-    });
+function loadSports() {
+ fetch('http://localhost:3000/sports').then(r => r.json()).then(appendItems)
+    sports.style.display = 'in-line'
+    function appendItems(x) {
+        const s = x['data'];
+        
+        const divElements = s.map(function(x){
+            const div = document.createElement('div')
+            div.id = `${x.id}`
+            div.innerText = `${x.attributes.title}`
+            return div
+        })
+        divElements.forEach(e => {
+            sports.appendChild(e)
+        });
+    }
 }
 
 //games fetch
 sports.addEventListener('click', (event) => {
     event.preventDefault()
     // console.log(event.target.id)
-    
     return fetch(`http://localhost:3000/sports/${event.target.id}/games`).then(r => r.json()).then(appendSports)
 
     function appendSports(x) {
+    
     sports.style.display = 'none'
     const newItem = document.createElement('div')
     newItem.id = 'game'
     const container = document.querySelector("#gameContainer")
     container.append(newItem)
     const games = x['data']
-    debugger;
     const liElements = games.map(function(x){
         const li = document.createElement('li')
         li.id = `${x.id}`
@@ -57,27 +60,30 @@ sports.addEventListener('click', (event) => {
 //odd fetch
 gameContainer.addEventListener('click', (event) => {
     console.log(event.target.id)
-
-    return fetch(`http://localhost:3000/sports/${event.target.id}/games`).then(r => r.json()).then(appendSports)
+    return fetch(`http://localhost:3000/games/${event.target.id}`).then(r => r.json()).then(appendSports)
 
     function appendSports(x) {
+    
     game.remove()
     const newItem = document.createElement('div')
     newItem.id = 'odds'
     const container = document.querySelector("#oddsContainer")
     container.append(newItem)
-    const games = x['data']
+    const odds = x['data']
+    for (const odd of odds.attributes.sites) {
+        console.log(odd.site_nice);
+        console.log(`${odd.odds['h2h'][0]}  ${odd.odds['h2h'][1]}`);
+        const div = document.createElement('div')
+        div.id = odd.site_key
+        div.innerText = `${odd.site_nice}: ${odd.odds['h2h'][0]}  ${odd.odds['h2h'][1]}`
+        newItem.appendChild(div)
+        return div
+    }
     debugger;
-    const liElements = games.map(function(x){
-        const li = document.createElement('li')
-        li.id = `${x.id}`
-        li.innerText = `${x.attributes.teams} ${x.attributes['sport_nice']}`
-        li.className = 'wrapper'
-        return li
-    });
-    liElements.forEach(e => {
-        newItem.appendChild(e)
-    });
+    
+    // tableElements.forEach(e => {
+    //     newItem.appendChild(e)
+    // });
     }
 })
 
@@ -86,8 +92,9 @@ gameContainer.addEventListener('click', (event) => {
 home.addEventListener('click', (e) => {
     console.log(e)
     e.preventDefault()
-    game.remove()
-    sports.style.display = 'block'
+    gameContainer.innerHTML = ''
+    oddsContainer.innerHTML = ''
+    container.style.display = 'flex'
     
 })
 
