6869014255d74c13b41f35070465f7cde716da0d
github username <github email address>
2021-04-16 19:26:02 -0400

    added constructor to sport class and loadSports and render functions

---
+++ src/sport.js
@@ -0,0 +1,34 @@
+class Sport {
+
+    static sports = []
+    static container = document.getElementById('container')
+
+    constructor({id, key, active, group, details, title}) {
+        this.id = id
+        this.key = key
+        this.active = active 
+        this.group = group 
+        this.details = details 
+        this.title = title 
+        this.div = document.createElement('div')
+
+        Sport.all.push(this)
+
+    }
+
+    loadSports() {
+        this.div.id = `${this.id}`
+        this.div.innerHTMl = `<div>
+                                <table>
+                                    <th>${this.details}</th>
+                                    <th>${this.active}</th>
+                                </table>
+                              </div>`
+        
+        return this.div
+    }
+
+    render() {
+        Sport.container.append(this.loadSports())
+    }
+}
