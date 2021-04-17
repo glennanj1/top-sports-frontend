43e25bed5711bd172b67b8694ec3b063851f9319
github username <github email address>
2021-04-17 11:47:41 -0400

    changed sports display structure and added event listener method for games.

--- src/sport.js
+++ src/sport.js
@@ -16,13 +16,33 @@ class Sport {
 
     }
 
+    
+
     loadSports() {
         this.div.id = `${this.id}`
-        this.div.innerHTML = `<li>${this.title}</li>`
+        this.div.innerHTML = `<li>
+                                ${this.title}
+                                ${this.group}
+                                ${this.details}
+                                ${this.active}
+                              </li>`
         return this.div
     }
 
     render() {
         Sport.container.append(this.loadSports())
+        this.event()
+    }
+
+    event(){
+        this.div.addEventListener('click', (e) => {
+            console.log(e.currentTarget.id)  
+            //remove sports
+            container.remove()
+            //append games function?
+            let id = e.currentTarget.id
+            const gameCall = new ApiCall(url, id)
+            gameCall.fetchGames()
+        })
     }
 }
