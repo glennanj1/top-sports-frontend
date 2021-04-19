c10fdd329b2564fd40fc63dedef85b0809c3ef69
github username <github email address>
2021-04-19 11:05:55 -0400

    added beturl constructor and fetch bets method

--- src/apiCall.js
+++ src/apiCall.js
@@ -3,7 +3,11 @@ class ApiCall {
     constructor(url, id) {
         this.sportUrl = `${url}/sports`
         this.gameUrl = `${url}/sports/${id}/games`
+        //show page
+        this.betUrl = `${url}/bets`
     }
+
+    
 
     fetchSports() {
         fetch(this.sportUrl).then(response => response.json()).then(data => {
@@ -23,4 +27,31 @@ class ApiCall {
             })
         })       
     }
+
+    fetchBets() {
+        const slip = {
+            amount:  amount.value,
+            date: date.value,
+            odds: odds.value,
+            game_id: game_id.value
+       }
+
+        const configObj = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            
+            body: JSON.stringify(slip)
+        
+        }
+
+        fetch(this.betUrl, configObj).then(r => r.json()).then(data => {
+            
+            const bet = new Bet({id: data['data'].id, ...data['data'].attributes})
+            debugger;
+            bet.render()
+            }) 
+        }
 } 
