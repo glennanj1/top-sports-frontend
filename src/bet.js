793790eb5c509d43969cc05663b154f62bc1a55b
github username <github email address>
2021-04-19 11:06:14 -0400

    started bet class

--- src/bet.js
+++ src/bet.js
@@ -1,3 +1,14 @@
 class Bet {
-    constructor()
+    constructor({amount, date, odds, game_id}) {
+        this.amount = amount 
+        this.date = date 
+        this.odds = odds 
+        this.game_id = game_id
+
+        this.div = document.createElement('div')
+    }
+
+    render() { alert('render this bet ;)')}
+    
 }
+
