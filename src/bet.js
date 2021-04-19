bc2ded51d1af0443cb75a3c65bcbf35df34a0ffc
github username <github email address>
2021-04-19 19:44:14 -0400

    added created_at and team, fixed innerhtml of div and added Placed Bet to bet container

--- src/bet.js
+++ src/bet.js
@@ -1,26 +1,40 @@
 class Bet {
-    constructor({amount, date, odds, game_id}) {
+
+
+    constructor({amount, date, odds, game_id, created_at, team}) {
         this.amount = amount 
         this.date = date 
         this.odds = odds 
         this.game_id = game_id
+        this.created_at = created_at
+        this.team = team
 
         this.div = document.createElement('div')
+
     }
+
+
 
     loadBet() { 
         //clear dom and render betslip
         gameContainer.innerHTML = ''
         betslipContainer.style.display = 'none'
         this.div.id = this.id 
-        this.div.innerHTML = `<h1>Bet Placed</h1>
-                              <li>Amount:${this.amount}</li>
-                              <li>Date:${this.date}</li>
-                              <li>Odds:${this.odds}</li>`
+        this.div.innerHTML = `<div>
+                                <h2>Amount: ${this.amount}</h2>
+                                <h2>Date: ${this.date}</h2>
+                                <h2>Odds: ${this.odds}</h2>
+                                <h2>Team: ${this.team}</h2>
+                                <h4>Placed at: ${this.created_at}</h4>
+                                `
+                              
        return this.div
     }
 
     render() {
+        let x = document.createElement('h1')
+        x.innerText = 'Placed Bet'
+        betContainer.append(x)
         betContainer.append(this.loadBet())
     }
     
