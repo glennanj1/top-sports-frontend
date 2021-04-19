6435a4fba16385ae6debe69077dfc5d8834b7556
github username <github email address>
2021-04-19 19:40:39 -0400

    added all bets event

--- index.js
+++ index.js
@@ -2,22 +2,34 @@ const url = 'http://localhost:3000'
 const apiCall = new ApiCall(url)
 // home button
 const home = welcome
-
+const allbets = document.getElementById('bets')
 
 //home button
 home.addEventListener('click', (e) => {
     console.log(e)
     e.preventDefault()
-    gameContainer.innerHTML = ''
+    Game.gameContainer.innerHTML = ''
     Game.gameContainer.style.width = '100%'
     oddsContainer.innerHTML = ''
     betslipContainer.style.display = 'none'
+    betContainer.innerHTML = ''
     apiCall.fetchSports()
+})
+//all bets button
+allbets.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(e)
+    container.innerHTML = ''
+    oddsContainer.innerHTML = ''
+    gameContainer.innerHTML = ''
+    betContainer.innerHTML = ''
+    apiCall.fetchAllBets()
 })
 
 //bet submit handler
 const betSubmit = (e) => { 
     e.preventDefault(),
+
     apiCall.fetchBets()
 }
 //betslip event
