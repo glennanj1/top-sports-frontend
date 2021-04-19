2ff9ccf7818eb8f82d6947bcb53ad88b83afcb50
github username <github email address>
2021-04-19 11:22:53 -0400

    added load bet method that renders placed bet and appends it

--- src/bet.js
+++ src/bet.js
@@ -8,7 +8,21 @@ class Bet {
         this.div = document.createElement('div')
     }
 
-    render() { alert('render this bet ;)')}
+    loadBet() { 
+        //clear dom and render betslip
+        gameContainer.innerHTML = ''
+        betslipContainer.style.display = 'none'
+        this.div.id = this.id 
+        this.div.innerHTML = `<h1>Bet Placed</h1>
+                              <li>Amount:${this.amount}</li>
+                              <li>Date:${this.date}</li>
+                              <li>Odds:${this.odds}</li>`
+       return this.div
+    }
+
+    render() {
+        betContainer.append(this.loadBet())
+    }
     
 }
 
